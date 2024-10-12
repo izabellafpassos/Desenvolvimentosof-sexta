5e686ef06faaa4512b7c047f2b8df664753592ac
izabellafpassos <izabellapasf@gmail.com>
2024-10-11 21:01:17 -0300

    aaa

--- Observer/observadorSecretaria.java
+++ Observer/observadorSecretaria.java
@@ -1,5 +1,15 @@
 package Observer;
 
-public class observadorSecretaria {
 
+public class observadorSecretaria extends absObserver {
+
+    //propriedade com o objeto observado
+    private nota nota_observada;//ponto de atençao o ideal é utilizar um contrato para 
+    //abstrair o objeto observado
+    //construtor
+    public observadorSecretaria(nota nota_observada) {
+        this.nota_observada = nota_observada;
+        this.nota_observada.attach(this);
+        //esse ojeto tem que se increver no observado 
+}
 }
--- Observer/teste.java
+++ Observer/teste.java
@@ -4,6 +4,7 @@ public class teste {
     public static void main(String[] args) {
         nota nota = new nota();
         observadorEstudante estudante = new observadorEstudante(nota);
+        observadorSecretaria secretaria = new observadorSecretaria(nota);
         nota.setSituacao("lançada");
     }
 }
